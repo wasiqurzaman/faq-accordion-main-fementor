7bc2e1740a6ac0968dd1adbdf083f07d782a5943
Wasiqur Zaman <wasiqurzaman@gmail.com>
2024-09-17 11:03:05 +0530

    Add script

---
+++ script.js
@@ -0,0 +1,21 @@
+const faqDivs = document.querySelectorAll(".faq");
+const openBtns = document.querySelectorAll(".btn-open");
+const answers = document.querySelectorAll("p");
+
+faqDivs.forEach((faq, i) => {
+  faq.addEventListener("click", function (e) {
+    if (faq.dataset.active === "false") {
+      faq.dataset.active = "true";
+      openBtns[i].src = "./assets/images/icon-minus.svg";
+      answers[i].style.display = "block";
+    }
+    else {
+      faq.dataset.active = "false";
+      openBtns[i].src = "./assets/images/icon-plus.svg";
+      answers[i].style.display = "none";
+    }
+  });
+});
+
+
+
