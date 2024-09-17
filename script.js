e01b4129f8640b0eab537a86b39d6785825283cf
Wasiqur Zaman <wasiqurzaman@gmail.com>
2024-09-17 13:13:29 +0530

    update

--- script.js
+++ script.js
@@ -2,9 +2,6 @@ const faqDivs = document.querySelectorAll(".faq");
 const openBtns = document.querySelectorAll(".btn-open");
 const answers = document.querySelectorAll("p");
 
-answers[0].style.display = "block";
-openBtns[i].src = "./assets/images/icon-minus.svg";
-
 faqDivs.forEach((faq, i) => {
   faq.addEventListener("click", function (e) {
     if (faq.dataset.active === "false") {
